50cfe066cc436a51b13575b2bc2a0d090d4f210f
stern <1536768420@qq.com>
2020-03-06 20:48:53 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by c happy on 2020/3/6.
+//  Copyright © 2020 c happy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
